9d13a09278d8ae71bebbdfee28aa464a5fdaf7cb
Roman Satanovskyi <Roman.Satanovskyi@mindgeek.com>
2019-10-11 10:34:57 +0300

    added component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import DummyComponent from 'es6-demo-react-npm2'
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+		<DummyComponent />
       </header>
     </div>
   );
